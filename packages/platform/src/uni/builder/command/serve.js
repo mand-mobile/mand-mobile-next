cbe3fcd5734f6bfcfe1a7ed5fa222ae15d1e3b25
msdlisper <1170167213@qq.com>
2020-07-19 20:06:59 +0800

    🐛 fix(scroller/build): can not find tsc
    Install the common package to the root package

--- packages/platform/src/uni/builder/command/serve.js
+++ packages/platform/src/uni/builder/command/serve.js
@@ -1,36 +1,41 @@
-
 const path = require('path')
 const Service = require('@vue/cli-service/lib/Service')
 
-
-const setUpEnv = (env) => {
-  Object.keys(env).forEach(key => process.env[key] = env[key])
+const setUpEnv = env => {
+  Object.keys(env).forEach(key => (process.env[key] = env[key]))
 }
 
 module.exports = (args, api) => {
-
   const {
-    exeRootPath, 
-    pluginRootPath, 
-    vueCliService, 
-    MAND_PLATFORM, 
+    exeRootPath,
+    pluginRootPath,
+    vueCliService,
+    MAND_PLATFORM,
     MAND_INPUT_DIR = '.mand-mobile',
-    MAND_OUTPUT_DIR = 'dist/mp-weixin/development' ,
-  } = (api.mdContext || {})
-  
+    MAND_OUTPUT_DIR = 'dist/mp-weixin/development',
+  } =
+    api.mdContext || {}
+
   const platformRootPath = path.resolve(__dirname, '../../../../')
 
   const {execa, info, error} = api.mdUtils
 
-  const NODE_PATH = `${path.resolve(exeRootPath, './node_modules')}:${path.resolve(platformRootPath, './node_modules')}:${path.resolve(pluginRootPath, './node_modules')}`
-  
-  //创建platform/node_modules软链接到src/node_modules下用于绕过uni对于包依赖加载路径的限制
+  const NODE_PATH = `${path.resolve(exeRootPath, './node_modules')}:${path.resolve(
+    platformRootPath,
+    './node_modules',
+  )}:${path.resolve(pluginRootPath, './node_modules')}`
+
+  // 创建platform/node_modules软链接到src/node_modules下用于绕过uni对于包依赖加载路径的限制
   execa('ln', ['-s', `${platformRootPath}/node_modules`, `${exeRootPath}/${MAND_INPUT_DIR}/node_modules`])
-  execa('ln', ['-s', `${exeRootPath}/node_modules/@mand-mobile/components`, `${exeRootPath}/${MAND_INPUT_DIR}/_mand-mobile`])
+  execa('ln', [
+    '-s',
+    `${exeRootPath}/node_modules/@mand-mobile/components`,
+    `${exeRootPath}/${MAND_INPUT_DIR}/_mand-mobile`,
+  ])
 
   const runner = execa(vueCliService, ['uni-build', '--watch'], {
     stdio: 'inherit',
-    env: { 
+    env: {
       FORCE_COLOR: true,
       // 运行npx vue-cli-service uni-serve 时需要指定以下两个环境变量
       NODE_ENV: 'development',
@@ -43,5 +48,4 @@ module.exports = (args, api) => {
       NODE_PATH,
     },
   })
-
 }
