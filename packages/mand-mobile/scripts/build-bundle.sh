05ca9e13e47ac744d4f6490a8919fddd79256e92
lucker <guoyunlong@didichuxing.com>
2020-08-10 18:29:55 +0800

    feat(platform): support postcss build

--- packages/mand-mobile/scripts/build-bundle.sh
+++ packages/mand-mobile/scripts/build-bundle.sh
@@ -8,6 +8,8 @@ export VUE_CLI_SKIP_DIRTY_GIT_PROMPT=true
 export MAND_INPUT_DIR=".mand-mobile/web-bundle"
 export MAND_OUTPUT_DIR="web/bundle"
 
+export MAND_PLATFORM="web"
+
 # vue invoke @mand-mobile/vue-cli-plugin-builder --platform web --target bundle
 
 VUE_CLI_SERVICE_CONFIG_PATH="$MAND_INPUT_DIR/vue.config.js" npx vue-cli-service md-build --platform=web --target=bundle $MAND_INPUT_DIR/main.js
