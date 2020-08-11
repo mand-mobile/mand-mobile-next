2f9fff0a85f46983b5c0387a1b778a3022236ebb
liuliang <liuliang@didiglobal.com>
2020-08-11 13:02:05 +0800

    🎸 feat(components/action-sheet): migrate action-sheet entry from platform
    migrate action-sheet entry from platform

--- packages/components/src/action-sheet/index.js
+++
@@ -1,4 +0,0 @@
-import ActionSheetFactory from '@mand-mobile/platform/lib/runtime/component/action-sheet'
-import ActionSheetOptions from './action-sheet'
-
-export default ActionSheetFactory(ActionSheetOptions)
