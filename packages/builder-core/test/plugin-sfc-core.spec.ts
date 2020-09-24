8fa44779aa4180960208c205ddb3d1039ea09015
lucker <guoyunlong@didichuxing.com>
2020-09-24 10:43:04 +0800

    test(builder-core): add test cases in sfc and rollup cases

--- packages/builder-core/test/plugin-sfc-core.spec.ts
+++ packages/builder-core/test/plugin-sfc-core.spec.ts
@@ -1,14 +1,16 @@
-import { RollupBuilderPlugin } from './../src/builtin-plugins/plugin-rollup-core';
+import { VueSFCBuilderPlugin } from './../src/builtin-plugins/plugin-sfc-core';
 import * as  path from 'path'
 import { BuilderContainer } from "../src/index";
 
 
-it('should build rollup bundle', async () => {
+it('should build sfc lib', async () => {
 
-  const outputRoot = path.resolve(__dirname, '__temp__/web-rollup-bundle')
+  const outputRoot = path.resolve(__dirname, '__temp__/src-compiler')
   const artifactRoot = path.resolve(outputRoot, 'dist')
   const builder = new BuilderContainer({outputRoot, artifactRoot, plugins: [
-    [RollupBuilderPlugin, {}],
+    [VueSFCBuilderPlugin, {
+      platform: 'web',
+    }],
   ]})
   await builder.create()
   await builder.build()
