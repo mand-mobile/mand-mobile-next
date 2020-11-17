f925b81ff9df91200d48cd17c775205ab6316069
xuxiaoyan <xuxiaoyan@didiglobal.com>
2020-11-17 16:15:21 +0800

    site:update entry & demo styles

--- .vuepress/theme/enhanceApp.js
+++ .vuepress/theme/enhanceApp.js
@@ -3,11 +3,17 @@ import Dialog from 'mand-mobile/lib/dialog/dialog'
 import Toast from 'mand-mobile/lib/toast/toast'
 import Icon from 'mand-mobile/lib/icon'
 
-Dialog.shadowRoot = document.head
-Toast.shadowRoot = document.head
-Icon.getRootNode = el => {
-  const root = el.getRootNode()
-  return root instanceof ShadowRoot ? root : document
+import VueClipboard from 'vue-clipboard2'
+
+const root = typeof window !== 'undefined' ? window : global
+
+if (root.document) {
+  Dialog.shadowRoot = root.document.head
+  Toast.shadowRoot = root.document.head
+  Icon.getRootNode = el => {
+    const root = el.getRootNode()
+    return root instanceof ShadowRoot ? root : root.document
+  }
 }
 
 // https://github.com/vuejs/vuepress/issues/1173#issuecomment-470534176
@@ -15,4 +21,5 @@ export default ({ Vue: _Vue, options }) => {
   for (const [name, component] of Object.entries(pageComponents)) {
     _Vue.component(name, component)
   }
+  _Vue.use(VueClipboard)
 }
