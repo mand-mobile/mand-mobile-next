d3fe964b2af528eca1af8b0c27daf7baf8ab9fb0
xuxiaoyan <xuxiaoyan@didiglobal.com>
2020-11-17 16:14:58 +0800

    site:update configration

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -29,6 +29,7 @@ module.exports = {
   title: 'Mand Mobile 3',
   base: '/mand-mobile/',
   head: [
+    ['link', { rel: 'icon', href: 'https://pt-starimg.didistatic.com/static/starimg/img/W8a5sNZUBC1605530382593.png' }],
     ['meta', { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge,chrome=1' }],
     ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover' }],
     ['meta', { name: 'format-detection', content: 'telephone=no,email=no' }],
@@ -37,7 +38,7 @@ module.exports = {
   ],
   themeConfig: {
     version: packageInfo.version,
-    logo: 'https://manhattan.didistatic.com/static/manhattan/mand/docs/mand-logo-black.svg',
+    logo: 'https://pt-starimg.didistatic.com/static/starimg/img/W8a5sNZUBC1605530382593.png',
     demoConfig: {
       shadowMode: false
     },
@@ -47,7 +48,7 @@ module.exports = {
         nav: [
           { text: '首页', link: '/' },
           { text: '组件', link: '/packages/components/', redirect: '/packages/components/development' },
-          { text: '平台', link: '/packages/platform', redirect: '/packages/platform' },
+          { text: '模块', link: '/packages/modules/', redirect: '/packages/modules/platform' },
           { text: '2.x', link: 'https://didi.github.io/mand-mobile' },
           { text: 'Github', link: 'https://github.com/mand-mobile/mand-mobile-next' },
         ],
@@ -62,29 +63,46 @@ module.exports = {
             }, 
             ...componentsSidebarConfig
           ],
-        },
-      },
-      '/en-US/': {
-        nav: [
-          { text: 'Home', link: '/' },
-          { text: 'Component', link: '/en-US/packages/components/', redirect: '/en-US/packages/components/development' },
-          { text: 'Platform', link: '/en-US/packages/platform', redirect: '/en-US/packages/platform' },
-          { text: '2.x', link: 'https://didi.github.io/mand-mobile' },
-          { text: 'Github', link: 'https://github.com/mand-mobile/mand-mobile-next' },
-        ],
-        sidebar: {
-          '/en-US/packages/components/': [
+          '/packages/modules/': [
             {
-              name: 'development',
-              title: 'Development Guide'
+              name: 'platform',
+              title: 'Platform'
             }, {
-              name: 'changelog',
-              title: 'Change Log'
-            }, 
-            ...componentsSidebarConfig
+              name: 'scroller',
+              title: 'Scroller'
+            }, {
+              name: 'shared',
+              title: 'Shared'
+            },
+            // {
+            //   name: 'Outbound',
+            //   title: 'test',
+            //   link: 'https://www.baidu.com'
+            // }, 
           ],
         },
-      }
+      },
+      // '/en-US/': {
+      //   nav: [
+      //     { text: 'Home', link: '/' },
+      //     { text: 'Component', link: '/en-US/packages/components/', redirect: '/en-US/packages/components/development' },
+      //     { text: 'Platform', link: '/en-US/packages/platform', redirect: '/en-US/packages/platform' },
+      //     { text: '2.x', link: 'https://didi.github.io/mand-mobile' },
+      //     { text: 'Github', link: 'https://github.com/mand-mobile/mand-mobile-next' },
+      //   ],
+      //   sidebar: {
+      //     '/en-US/packages/components/': [
+      //       {
+      //         name: 'development',
+      //         title: 'Development Guide'
+      //       }, {
+      //         name: 'changelog',
+      //         title: 'Change Log'
+      //       }, 
+      //       ...componentsSidebarConfig
+      //     ],
+      //   },
+      // }
     }
     // sidebarDepth: 2
   },
