8edae3a6c46be6cbafe49883575f2fc3e8e892d4
xuxiaoyan <xuxiaoyan@didiglobal.com>
2020-12-31 16:38:32 +0800

    💡 refactor(components/roller-success): add uni support

---
+++ packages/components/src/activity-indicator/roller-success.uni.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="md-activity-indicator-rolling-success">
+    <img v-if="!isSuccess" class="md-activity-indicator-rolling-success_image" src="https://pt-starimg.didistatic.com/static/starimg/img/IHcr0BomhJ1609333095846.gif" alt="">
+    <img v-else class="md-activity-indicator-rolling-success_image" src="https://pt-starimg.didistatic.com/static/starimg/img/DRz83ufJVh1609333095729.gif" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isSuccess: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+
+</script>
+
+<style lang="stylus">
+.md-activity-indicator-rolling-success
+  width 100%
+  height 100%
+  &_image
+    display block
+    width 100%
+    height 100%
+</style>
