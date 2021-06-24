5b99a1402c1cf02a3894d4cea2ccf9c83125cfd0
zouhang <zouhang@didiglobal.com>
2021-06-24 18:01:02 +0800

    test: ignore some code test

--- src/packages/radio-list/index.ts
+++ src/packages/radio-list/index.ts
@@ -1,6 +1,7 @@
 import type { App } from 'vue'
 import { RadioList } from '../radio'
 
+/* istanbul ignore next */
 RadioList.install = (app: App) => {
   app.component(RadioList.name, RadioList)
 }
