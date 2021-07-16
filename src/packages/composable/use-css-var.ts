8fa99b59c686f9eecd4e11fd20cf7cf7328d8a34
zouhang <zouhang@didiglobal.com>
2021-07-16 18:14:24 +0800

    perf: optimize usecssvar logic

--- src/packages/composable/use-css-var.ts
+++ src/packages/composable/use-css-var.ts
@@ -4,24 +4,57 @@ import {
   onMounted,
   computed,
   unref,
+  inject,
+  onUnmounted,
+  provide,
+  ref,
 } from 'vue'
-import type { Ref, WatchStopHandle } from 'vue'
+import type {
+  Ref,
+  WatchStopHandle,
+  InjectionKey,
+} from 'vue'
 
-let stopWatchCssVar: WatchStopHandle | null = null
+type VarsType =
+  | Ref<Record<string, string>>
+  | Record<string, string>
+
+const VAR_PREFIX = '--md-'
+
+const setVars = (
+  target: HTMLElement,
+  val: Record<string, string>
+) => {
+  Object.keys(val).forEach((key) => {
+    if (key.startsWith(VAR_PREFIX)) {
+      target?.style.setProperty(key, val[key])
+    } else {
+      target?.style.setProperty(VAR_PREFIX + key, val[key])
+    }
+  })
+}
+
+export const themeVarsKey: InjectionKey<VarsType> =
+  'themeVars' as any
 
 /**
+ * @param vars
+ * @param target
+ * @returns stopWatchHandler
+ * @example
+ *
  * If you pass a Ref vars param, it will generate a watch handler.
  *
  * In main.ts:
  *
  * ```ts
- * const themeVars ={
+ * const themeVars = {
  *  '--md-color-primary': '#f44336',
  *  '--md-color-white': '#2196f3',
  * }
- * useCssVar(themeVars)
+ * app.provide(themeVarsKey, themeVars)
  * ```
- *
+ * Usually you need to use this function in the root component.
  * In components' steup:
  *
  * ```ts
@@ -30,56 +63,49 @@ let stopWatchCssVar: WatchStopHandle | null = null
  *    '--md-button-default-background-color': '#f44336',
  *    '--md-button-default-font-color': '#2196f3',
  *  })
- *  const stop = useCssVar(themeVars)
- *
- *  onUnmount(() => {
- *   stop()
- *  })
+ *  useCssVar(themeVars)
  * }
  * ```
  */
 export function useCssVar(
-  vars:
-    | Ref<Record<string, string>>
-    | Record<string, string>,
+  vars: VarsType,
   target?: Ref<HTMLElement> | HTMLElement
 ) {
+  let stopWatchCssVar: WatchStopHandle | null = null
+
   const elRef = computed(
     () => unref(target) || window?.document?.documentElement
   )
-  const VAR_PREFIX = '--md-'
 
-  const setVars = (
-    target: HTMLElement,
-    val: Record<string, string>
-  ) => {
-    Object.keys(val).forEach((key) => {
-      if (key.startsWith(VAR_PREFIX)) {
-        target.style.setProperty(key, val[key])
-      } else {
-        target.style.setProperty(VAR_PREFIX + key, val[key])
-      }
-    })
-  }
+  const themeVars = useThemeVars()
+  const customVars = { ...themeVars, ...unref(vars) }
 
-  if (stopWatchCssVar) {
-    stopWatchCssVar()
-    stopWatchCssVar = null
-  }
+  provide(themeVarsKey, ref(customVars))
 
   onMounted(() => {
     isRef(vars)
       ? (stopWatchCssVar = watch(
           vars,
           (val) => {
-            setVars(elRef.value, val)
+            setVars(elRef.value, {
+              ...unref(themeVars),
+              ...val,
+            })
           },
           {
             immediate: true,
           }
         ))
-      : setVars(elRef.value, vars)
+      : setVars(elRef.value, {
+          ...unref(themeVars),
+          ...vars,
+        })
   })
 
-  return stopWatchCssVar
+  onUnmounted(() => stopWatchCssVar && stopWatchCssVar())
+}
+
+export const useThemeVars = () => {
+  const themeVars = inject(themeVarsKey, {})
+  return themeVars
 }
