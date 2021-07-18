1bee94ab8e4101092ecc3d979683333d714db216
zouhang <zouhang@didiglobal.com>
2021-07-18 12:12:39 +0800

    feat: add change color primary

---
+++ docs/.vitepress/theme/components/ThemeSwitch.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useCssVar } from 'mand-mobile/composable'
+
+const theme = ref ({
+  'color-primary': '#4280EB'
+})
+
+useCssVar(theme)
+</script>
+<template>
+  <input v-model="theme['color-primary']" type="color" class="color-picker" />
+</template>
+
+<style lang="stylus">
+.color-picker
+  padding 0
+  width 20px
+  height 24px
+  border none
+  outline none
+  background transparent
+  cursor pointer
+</style>
