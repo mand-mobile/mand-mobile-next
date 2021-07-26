f254c616c9d7217cfd0d353e6892599ba7f40d6f
zouhang <zouhang@didiglobal.com>
2021-07-26 16:34:08 +0800

    feat(tip): support content slot

--- src/packages/tip/demo/demo3.vue
+++ src/packages/tip/demo/demo3.vue
@@ -8,7 +8,6 @@ export default {
 import MdTip from 'mand-mobile-next/tip'
 import MdButton from 'mand-mobile-next/button'
 import Toast from 'mand-mobile-next/toast'
-import { ref } from 'vue'
 
 const showHandler = () => {
   console.log('Tip show')
@@ -16,19 +15,29 @@ const showHandler = () => {
 const hideHandler = () => {
   Toast.info('hide click')
 }
+const showCoupon = () => {
+  Toast.succeed('已领取优惠券！')
+}
 </script>
 
 <template>
   <md-tip
     name="md-tip-4"
     icon="security"
-    content="完善信息，领取5元免息券"
     :offset="{ top: -10, left: 0 }"
     fill
     :click-outside-hide="false"
     @show="showHandler"
     @hide="hideHandler"
   >
+    <template #content>
+      <div class="content">
+        完善信息，领取5元
+        <md-button :type="'link'" @click="showCoupon">
+          免息券
+        </md-button>
+      </div>
+    </template>
     <md-button>click me</md-button>
   </md-tip>
 </template>
@@ -38,6 +47,9 @@ const hideHandler = () => {
   color #C37447
   background-color #FFEBDF
   box-shadow none
+  .content
+    flex 1
+    font-size 28px
   .tip-arrow
     border-width 10px 11px 0 11px
     border-color #FFEBDF transparent transparent transparent
