89844100a29bdcdd375d31e69f73d6b080119121
zouhang <zouhang@didiglobal.com>
2021-07-29 16:52:33 +0800

    docs(action-sheet): update zh-cn docs

---
+++ docs/.vitepress/theme/components/Badge.vue
@@ -0,0 +1,11 @@
+<template>
+  <span class="badge p-1 text-light-300 rounded-md text-xs"><slot/></span>
+</template>
+
+<style>
+.badge {
+  position: relative;
+  top: -2px;
+  background-color: var(--md-color-primary);
+}
+</style>
