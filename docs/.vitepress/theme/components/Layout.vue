c0964aa5f867c14b62e7856fe823c08470ba4341
zouhang <zouhang@didiglobal.com>
2021-08-16 19:22:52 +0800

    docs: mounted  teleport after 1000ms

--- docs/.vitepress/theme/components/Layout.vue
+++ docs/.vitepress/theme/components/Layout.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
+import { Teleport, onMounted, ref } from 'vue'
 import Layout from 'fisand-doc/dist/client/theme-default/Layout.vue'
-import { Teleport } from 'vue'
 import ThemeSwitch from './ThemeSwitch.vue'
 import QrCode from './QrCode.vue'
+
+const showTeleport = ref(false)
+
+onMounted(() => {
+  setTimeout(() => {
+    showTeleport.value = true
+  }, 1000)
+})
 </script>
 <template>
   <Layout />
-  <Teleport to=".nav-icons">
-    <QrCode />
-  </Teleport>
-  <Teleport to=".nav-icons">
-    <ThemeSwitch />
-  </Teleport>
+  <ClientOnly v-if="showTeleport">
+    <Teleport to=".nav-icons">
+      <QrCode />
+    </Teleport>
+    <Teleport to=".nav-icons">
+      <ThemeSwitch />
+    </Teleport>
+  </ClientOnly>
 </template>
